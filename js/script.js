9dd687f137297a90d2c85743eaeba410c0ab27c9
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-03-21 14:59:50 +0000

    I used js to calculate math

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function doMathClicked() {
+    document.getElementById('multiply-math').innerHTML = "2(6 * 12) = " + (2 * (6 * 12))
+     document.getElementById('add-math').innerHTML = "(6 + 12) = " + (6+12)
+}
